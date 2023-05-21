3e88f82f340da44575443522603caef648a6473a
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-21 20:11:49 +0200

    Add path to position_list page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
-from task_manager.views import index, TaskListView, WorkerListView
+from task_manager.views import(
+    index,
+    TaskListView,
+    WorkerListView,
+    PositionListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("positions/", PositionListView.as_view(), name="position-list"),
 ]
 
 app_name = "task_manager"
