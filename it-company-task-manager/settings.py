e5788d6689a7f06839a54807fcab83239e55c760
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-21 15:08:21 +0200

    Add app name task_manager to settings.py

--- it-company-task-manager/settings.py
+++ it-company-task-manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "task_manager",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_manager.Worker"
+
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
