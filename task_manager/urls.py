e8b3392155ccfb108763731348b8f56f5ad14652
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-21 19:50:36 +0200

    Add path to worker_list page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from task_manager.views import index, TaskListView
+from task_manager.views import index, TaskListView, WorkerListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
 ]
 
 app_name = "task_manager"
