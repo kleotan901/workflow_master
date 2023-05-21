1d71118249cd74b142ecba16894f5aec47853f54
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-21 18:21:44 +0200

    Add function index to views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,31 @@
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Position, Worker, Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_tasks": num_tasks,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    ordering = ["last_name"]
+    paginate_by = 5
+
+
+
+
+
