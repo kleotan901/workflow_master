d7f85089a95ea000187659320b5ea51834f3c0a3
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-22 11:11:37 +0200

    Add path to task_detail page to urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
-from task_manager.views import(
+from task_manager.views import (
     index,
     TaskListView,
     WorkerListView,
-    PositionListView
+    PositionListView,
+    TaskDetailView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("positions/", PositionListView.as_view(), name="position-list"),
 ]
