1d5ee28f9dcc2795e9b9c2447e80bc97a0121086
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-23 17:55:05 +0200

    Add path to task_confirm_delete page to urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -9,7 +9,8 @@ from task_manager.views import (
     TaskDetailView,
     WorkerDetailView,
     TaskCreateView,
-    TaskUpdateView
+    TaskUpdateView,
+    TaskDeleteView
 )
 
 urlpatterns = [
@@ -18,6 +19,7 @@ urlpatterns = [
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("positions/", PositionListView.as_view(), name="position-list"),
