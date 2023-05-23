182f727fe4d288a4861363b22d9019ea16384638
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-23 17:33:49 +0200

    Add path to task_update page to urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,7 +7,9 @@ from task_manager.views import (
     WorkerListView,
     PositionListView,
     TaskDetailView,
-    WorkerDetailView, TaskCreateView,
+    WorkerDetailView,
+    TaskCreateView,
+    TaskUpdateView
 )
 
 urlpatterns = [
@@ -15,6 +17,7 @@ urlpatterns = [
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("positions/", PositionListView.as_view(), name="position-list"),
