81265a3374f032d135ad606ec7a7e91f1a419930
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-23 12:59:21 +0200

    Set settings for debug_toolbar and aply black

--- it-company-task-manager/urls.py
+++ it-company-task-manager/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("task_manager/", include("task_manager.urls", namespace="task_manager"))
-
+    path("admin/", admin.site.urls),
+    path("task_manager/", include("task_manager.urls", namespace="task_manager")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
