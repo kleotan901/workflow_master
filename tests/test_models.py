616632de6d06ea7ebe4837ca32e09431dc59fd61
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-12 14:07:19 +0200

    Add tests for TaskType and Tag models
    - Update test_models.py to include tests for TaskType and Tag models
    - add test_views_tag.py to include relevant tests
    - add test_views_task_type.py to include relevant tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from task_manager.models import Position, TaskType, Task
+from task_manager.models import(
+    Position,
+    TaskType,
+    Task,
+    Tag
+)
 
 
 class ModelsTest(TestCase):
@@ -29,3 +34,11 @@ class ModelsTest(TestCase):
             task_type=task_type
         )
         self.assertEqual(str(task), f"{task.name} {task.deadline}")
+
+    def test_type_task_str(self):
+        type_task = TaskType.objects.create(name="Test type_task")
+        self.assertEqual(str(type_task), type_task.name)
+
+    def test_tag_str(self):
+        tag = TaskType.objects.create(name="Test tag")
+        self.assertEqual(str(tag), tag.name)
---
+++ tests/test_views_tag.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from task_manager.models import Tag
+
+
+TAG_TYPE_LIST_URL = reverse("task_manager:tag-list")
+
+
+class PublicTagTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_task_type_list_login_required(self):
+        response = self.client.get(TAG_TYPE_LIST_URL)
+        self.assertNotEqual(response.status_code, 200)
+        self.assertRedirects(response, "/accounts/login/?next=/task_manager/tags/")
+
+
+class PrivateTagTest(TestCase):
+    def setUp(self) -> None:
+        for tag_id in range(8):
+            Tag.objects.create(name=f"Test {tag_id}")
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test password"
+        )
+        self.client.force_login(self.user)
+
+    def test_tag_pagination_is_five(self):
+        response = self.client.get(TAG_TYPE_LIST_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertEqual(len(response.context["tag_list"]), 5)
+        self.assertTemplateUsed(response, "task_manager/tag_list.html")
+
+    def test_tag_pagination_second_page(self):
+        response = self.client.get(TAG_TYPE_LIST_URL + "?page=2")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertEqual(len(response.context["tag_list"]), 3)
+        self.assertTemplateUsed(response, "task_manager/tag_list.html")
+
+    def test_create_tag(self):
+        form_data = {
+            "name": "test new_tag"
+        }
+        response = self.client.post(
+            reverse("task_manager:tag-create"),
+            data=form_data
+        )
+        task_type = Tag.objects.get(name=form_data["name"])
+
+        self.assertEqual(task_type.name, form_data["name"])
+        self.assertRedirects(response, "/task_manager/tags/")
+
+    def test_delete_tag(self):
+        tag = Tag.objects.get(pk=1)
+        response = self.client.post(
+            reverse("task_manager:tag-delete", kwargs={"pk": tag.id})
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(
+            Tag.objects.filter(pk=tag.id).exists()
+        )
+
+    def test_tag_search_matches_found(self):
+        response = self.client.get("/task_manager/tags/?name=Test+1")
+        searching_manufacturer = Tag.objects.filter(name="Test 1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["tag_list"]),
+            list(searching_manufacturer)
+        )
+
+    def test_tag_search_no_matches_found(self):
+        response = self.client.get("/task_manager/tags/?name=Fake+name")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "There are no tags"
+        )
+
+    def test_pagination_tag_search_with_value_current_page(self):
+        response = self.client.get("/task_manager/tags/?name=Test")
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertEqual(len(response.context["tag_list"]), 5)
+
+    def test_pagination_tag_search_with_value_next_page(self):
+        response = self.client.get("/task_manager/tags/?name=Test&page=2")
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertEqual(len(response.context["tag_list"]), 3)
