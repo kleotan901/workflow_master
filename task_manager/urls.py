d02faaa23e39b3dbbe71ab4c5db349b8ec9f62bd
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-17 14:57:59 +0200

    Change values in paths pk to slug in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -33,33 +33,33 @@ urlpatterns = [
     path("", HomePage.as_view(), name="index"),
     path("register/", RegisterWorker.as_view(), name="register"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
-    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<slug:slug>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<slug:slug>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<slug:slug>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path(
-        "tasks/<int:pk>/toggle-assign/",
+        "tasks/<slug:slug>/toggle-assign/",
         ToggleAssignToTaskView.as_view(),
         name="toggle-task-assign",
     ),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
-    path(
-        "workers/<int:pk>/",
-         WorkerDetailView.as_view(),
-         name="worker-detail"
-    ),
     path(
         "workers/create/",
         WorkerCreateView.as_view(),
         name="worker-create"
     ),
     path(
-        "workers/<int:pk>/update/",
+        "workers/<slug:slug>/",
+         WorkerDetailView.as_view(),
+         name="worker-detail"
+    ),
+    path(
+        "workers/<slug:slug>/update/",
         WorkerUpdateView.as_view(),
         name="worker-update"
     ),
     path(
-        "workers/<int:pk>/delete/",
+        "workers/<slug:slug>/delete/",
         WorkerDeleteView.as_view(),
         name="worker-delete"
     ),
